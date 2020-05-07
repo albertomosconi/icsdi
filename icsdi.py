bdb37be12dd7996597e0600907d3c44272f41d2d
Alberto Mosconi <pitagora99@gmail.com>
2020-05-07 23:10:06 +0200

    basic parser

--- icsdi.py
+++ icsdi.py
@@ -174,12 +174,96 @@ class Lexer:
 
 
 ########################
+# NODES
+########################
+
+class NumberNode:
+    'syntax tree node that holds either INT or FLOAT numbers'
+
+    def __init__(self, token):
+        self.token = token
+
+    def __repr__(self):
+        'display node'
+        return f'{self.token}'
+
+
+class BinOpNode:
+    'syntax tree node for binary operators: +, -, etc'
+
+    def __init__(self, left_node, op_token, right_node):
+        self.left_node = left_node
+        self.op_token = op_token
+        self.right_node = right_node
+
+    def __repr__(self):
+        'display node'
+        return f'({self.left_node}, {self.op_token}, {self.right_node})'
+
+
+########################
+# PARSER
+########################
+
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.token_idx = -1
+        self.advance()
+
+    def advance(self):
+        self.token_idx += 1
+        if self.token_idx < len(self.tokens):
+            self.current_token = self.tokens[self.token_idx]
+        return self.current_token
+
+    def parse(self):
+        res = self.expr()
+        return res
+
+    def factor(self):
+        'returns NumberNode if the current token is an INT or FLOAT'
+        tok = self.current_token
+
+        if tok.type in (TT_INT, TT_FLOAT):
+            self.advance()
+            return NumberNode(tok)
+
+    def term(self):
+        'evaluates term'
+        return self.binary_operation(self.factor, (TT_MUL, TT_DIV))
+
+    def expr(self):
+        'evaluates expression'
+        return self.binary_operation(self.term, (TT_PLUS, TT_MINUS))
+
+    def binary_operation(self, func, ops):
+        'returns BinOpNode for allowed operators (ops) using "func" to get the left and right nodes'
+        left = func()
+
+        while self.current_token.type in ops:
+            op_token = self.current_token
+            self.advance()
+            right = func()
+            left = BinOpNode(left, op_token, right)
+
+        return left
+
+
+########################
 # RUN
 ########################
 
+
 def run(fn, text):
-    'runs lexer on input text and returns list of tokens and error'
+    'generate tokens'
     lexer = Lexer(fn, text)
     tokens, error = lexer.make_tokens()
+    if error:
+        return None, error
 
-    return tokens, error
+    'generate AST (abstract syntax tree)'
+    parser = Parser(tokens)
+    ast = parser.parse()
+
+    return ast, None
